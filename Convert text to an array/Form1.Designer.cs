c0cafa3b460615be31bc55ea6c6f3ad1b6bc37be
Owen Quinlan <46948241+RelievedStudios@users.noreply.github.com>
2019-05-30 21:00:06 +1000

    init

---
+++ Convert text to an array/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Convert_text_to_an_array
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 21);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 206);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "abcdefghijklmnopqrstuvwxyz";
+            // 
+            // convertButton
+            // 
+            this.convertButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.convertButton.Location = new System.Drawing.Point(12, 233);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(776, 136);
+            this.convertButton.TabIndex = 1;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.Location = new System.Drawing.Point(12, 375);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(776, 206);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 591);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Text to array";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ Convert text to an array/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Convert_text_to_an_array
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ConvertButton_Click(object sender, EventArgs e)
+        {
+            String inputText = richTextBox1.Text.ToUpper();
+            Char[] inputChars = inputText.ToCharArray();
+
+            int i = 1;
+            int x = 0;
+            string outputText = "";
+            while (inputText.Length > x-1)
+            {
+
+                outputText = outputText + "["+ getCharSafe(inputText.ToCharArray(), x)+
+                    ", " + getCharSafe(inputText.ToCharArray(), x +1) +
+                    ", " + getCharSafe(inputText.ToCharArray(), x +2) +
+                    ", " + getCharSafe(inputText.ToCharArray(), x +3) + "],\n";
+
+                x = x + 4;
+                i++;
+            }
+            richTextBox2.Text = outputText;
+        }
+        public string getCharSafe(Char[] input, int run)
+        {
+            if (run < input.Length)
+            {
+                if (input[run].ToString() == " ")
+                {
+                    return "_";
+                }
+                else
+                    return input[run].ToString();
+            }
+            else return "_";
+        }
+    }
+
+}
